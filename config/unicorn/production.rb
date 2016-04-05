52f10e74b8a9c8c8ba19e268c3b6f2246948c627
Alex Stepanenko <stepanenko.aleksander@gmail.com>
2016-04-05 17:55:20 +0300

    fix

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,9 +3,9 @@ worker_processes 1
 APP_DIR = '/home/alex/projects/simple_blog'
 working_directory APP_DIR + '/current'
 
-listen APP_DIR + '/shared/tmp/unicorn.sock', backlog: 64
- 
 pid APP_DIR + '/shared/tmp/pids/unicorn.pid'
+
+listen APP_DIR + '/shared/tmp/unicorn.sock', backlog: 64
 
 stderr_path APP_DIR + '/shared/log/unicorn.stderr.log'
 stdout_path APP_DIR + '/shared/log/unicorn.stdout.log'
