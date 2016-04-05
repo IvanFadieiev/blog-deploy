055ffb492d796bc8238cd37cd2e1d50b1add8b30
Alex Stepanenko <stepanenko.aleksander@gmail.com>
2016-04-05 17:53:12 +0300

    backlog

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,7 +4,7 @@ APP_DIR = '/home/alex/projects/simple_blog'
 working_directory APP_DIR + '/current'
 
 listen APP_DIR + '/shared/tmp/unicorn.sock', backlog: 64
-
+ 
 pid APP_DIR + '/shared/tmp/pids/unicorn.pid'
 
 stderr_path APP_DIR + '/shared/log/unicorn.stderr.log'
